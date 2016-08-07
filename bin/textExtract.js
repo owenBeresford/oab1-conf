d0663974dd88c26e19cda4fec63236cbd13cdca2
oab1 <owenberesford@fastmail.fm>
2016-08-07 15:32:27 +0100

    Improve recursive descent && add JS literal extraction

--- bin/textExtract.js
+++ bin/textExtract.js
@@ -4,18 +4,18 @@
 // hide code  :-)
 (function() {
 
-const RECURSE_DEPTH=15;
+const RECURSE_DEPTH=4;
 
-display(JSON.stringify(filter2(filter1(extract())), null, "\t") );
+display(JSON.stringify(filter2(filter1(filter3(extractText()).concat(extractJS()))), null, "\t") );
 
 
-function extract() {
-	var offset=1;
+function extractText() {
+	var offset=0;
 	var buf=[];
-	jQuery('body').each(extractRecurseMap);
+	jQuery('body').each(function(indx, ele) {return extractRecurseMap(indx, ele, 0); } );
 
-	function extractRecurseMap(indx, ele ) {
-		if(buf.length> RECURSE_DEPTH) {
+	function extractRecurseMap(indx, ele, DEPTH ) {
+		if(DEPTH> RECURSE_DEPTH) {
 			return;
 		}
 
@@ -25,15 +25,28 @@ function extract() {
 			buf[offset]=t1;
 			offset=offset+1;
 		}
-		$(ele).find('*').each(extractRecurseMap);
+		$(ele).find('*').each(function(indx, ele) { return extractRecurseMap(indx, ele, DEPTH+1); });
 	}
 	return buf;
 }
 
+function extractJS() {
+	var buf=[];
+	jQuery('script').each(extractTextLiterals);
+
+	function extractTextLiterals(indx, ele) {
+		var t=$(ele).text();
+		var str=t.match(/'([^']+)'/g);
+		buf.concat(str);
+		str2=t.match(/"([^"]+)"/g);
+		buf.concat(str2);
+	}
+	return buf;
+}
 
 function filter1(data) {
 	data=data.sort();
-	keys=[];
+	var keys=[];
 	for (var i = 0; i < data.length; i++) {
 		if (keys.indexOf( data[i])==-1 ) {
 			keys.push( data[i] );
@@ -49,9 +62,20 @@ function filter2(data) {
 	return data;
 }
 
+function filter3(data) {
+	var out=[];
+	for (var i = 0; i < data.length; i++) {
+		if(data[i].match(/<.+>/) == null) {
+			out[out.length]=data[i];
+		}
+	}
+	return out;
+}
+
 function display(data) {
 	var out = window.open("", "JSON array", "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=780, height=200, top="+(screen.height-400)+", left="+(screen.width-840));
 	out.document.body.innerHTML = "<textarea style=\"width:90%; height:90%; margin:auto;\">"+data+"</textarea>";
 	return 0;
 }
 })();
+
